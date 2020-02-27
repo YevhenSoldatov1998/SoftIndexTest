ad98106f77b8b4b0314c3723beb416f3a5803555
Yevhen <arr.ess1998@gmail.com>
2020-02-27 08:32:01 +0200

    table modified

--- src/component/Table/Table.tsx
+++ src/component/Table/Table.tsx
@@ -1,20 +1,24 @@
-import React, {FC} from 'react'
+import React, {ChangeEvent, FC} from 'react'
 import {UserType} from '../../types/types'
 
 type PropsType = {
     users: Array<UserType>
 }
 const Table: FC<PropsType> = ({users}) => {
+    const handleSort = (e: any) => {
+        debugger
+        let current = e.target.id
+    }
     return (
         <table>
             <caption>Users</caption>
             <thead>
-            <tr>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Gender</th>
-                <th scope="col">Age</th>
+            <tr onClick={handleSort}>
+                <th id="firstName" scope="col">First Name</th>
+                <th id="lastName" scope="col">Last Name</th>
+                <th id="phone" scope="col">Phone</th>
+                <th id="gender" scope="col">Gender</th>
+                <th id="age" scope="col">Age</th>
             </tr>
             </thead>
             <tbody>
